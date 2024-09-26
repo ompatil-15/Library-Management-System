c1bb96a36f86dbd01d8a61bc9def4ec272ee8b35
Om Patil <patilom001@gmail.com>
2024-09-26 22:20:19 +0530

    Implement borrow book feature

--- library.py
+++ library.py
@@ -12,3 +12,12 @@ class Library:
 
     def add_book(self, book):
         self.books.append(book)
+
+    def borrow_book(self, book_id):
+        for book in self.books:
+            if book.book_id == book_id:
+                if book.is_borrowed:
+                    raise Exception("Book is already borrowed")
+                book.is_borrowed = True
+                return
+        raise Exception("Book not found")
---
+++ test_library.py
@@ -0,0 +1,27 @@
+import unittest
+from library import Library, Book
+
+class TestLibrary(unittest.TestCase):
+    def test_add_book(self):
+        library = Library()
+        book =  Book("B001", "Zero to One", "Peter Thiel", 2014)
+        library.add_book(book)
+        self.assertIn(book, library.books)
+    def test_borrow_book(self):
+        library = Library()
+        book = Book("123", "The Great Gatsby", "F. Scott Fitzgerald", 1925)
+        library.add_book(book)
+        library.borrow_book("123")
+        self.assertTrue(book.is_borrowed)
+
+    def test_borrow_unavailable_book(self):
+        library = Library()
+        book = Book("123", "The Great Gatsby", "F. Scott Fitzgerald", 1925)
+        library.add_book(book)
+        library.borrow_book("123")
+        with self.assertRaises(Exception):
+            library.borrow_book("123")
+
+
+if __name__ == '__main__':
+    unittest.main()
